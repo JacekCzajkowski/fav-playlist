e4594706341974bd45585d2e3e49afd02f3222e8
Jacek <ja_cek@gmx.com>
2021-04-28 23:24:02 +0200

    project update

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,10 +1,29 @@
 <template>
-  <h2>Playlist Details. ID - {{ id }}</h2>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl">
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import getDocument from '@/composables/getDocument'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  setup(props) {
+    const { error, document: playlist } = getDocument('playlists', props.id)
+
+    return { error, playlist }
+  }
 }
 </script>
 
