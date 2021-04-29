a04907f3fd61b625b7f77423e7e0b6f7a11a5f98
Jacek <ja_cek@gmx.com>
2021-04-29 13:02:53 +0200

    project update

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -11,6 +11,10 @@
       <p class="description">{{ playlist.description }}</p>
     </div>
 
+    <div class="song-list">
+      <p>song list here</p>
+    </div>
+
   </div>
 </template>
 
@@ -28,5 +32,42 @@ export default {
 </script>
 
 <style>
-
+  .playlist-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 80px;
+  }
+  .cover {
+    overflow: hidden;
+    border-radius: 20px;
+    position: relative;
+    padding: 160px;
+  }
+  .cover img {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 200%;
+    max-height: 200%;
+  }
+  .playlist-info {
+    text-align: center;
+  }
+  .playlist-info h2 {
+    text-transform: capitalize;
+    font-size: 28px;
+    margin-top: 20px;
+  }
+  .playlist-info p {
+    margin-bottom: 20px;
+  }
+  .username {
+    color: #999;
+  }
+  .description {
+    text-align: left;
+  }
 </style>
