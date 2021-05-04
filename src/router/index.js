77ebeb7dd3d93687771d4bee39db9a484bbd839b
Jacek <ja_cek@gmx.com>
2021-05-04 12:53:26 +0200

    project update

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
 import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
 import PlaylistDetails from '../views/playlists/PlaylistDetails.vue'
+import UserPlaylists from '../views/playlists/UserPlaylists.vue'
 
 
 import { projectAuth } from '../firebase/config'
@@ -46,7 +47,12 @@ const routes = [
     component: PlaylistDetails,
     props: true,
     beforeEnter: requireAuth
-    
+  },
+  {
+    path: '/playlists/user',
+    name: 'UserPlaylists',
+    component: UserPlaylists,
+    beforeEnter: requireAuth
   }
 ]
 
